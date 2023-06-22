c4f9c8bd3bb5d1135f57454dc1eb52253d991779
007chiragshah <chirags603@gmail.com>
2023-06-22 10:27:33 +0000

    Updating ansible files

---
+++ modules/script_module/testscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pwd >> /tmp/outputfromscript3.txt
+
+echo "first arg: $1" >> /tmp/outputfromscript3.txt
+
+echo "==================================================" >> /tmp/outputfromscript3.txt
+
+ifconfig >> /tmp/outputfromscript3.txt
